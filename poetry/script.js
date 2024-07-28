7ce0760412a64f93c78017449d60057645ec9ce0
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-07-28 16:09:27 +0200

    displaying all poems

--- poetry/script.js
+++ poetry/script.js
@@ -12,8 +12,41 @@ class Model {
 }
 
 class View {
-    constructor() { 
+    constructor() {
         this.poemsDiv = document.getElementById("poemsDiv");
+    }
+
+    bindLoadPoems(handler) {
+        window.addEventListener("load", (event) => {
+            event.preventDefault();
+            handler()
+        })
+    }
+
+    clearPoemsDiv() {
+        this.poemsDiv.innerHTML = '<h2 class="mb-5">poems</h2>'
+    }
+
+    addPoem(poem) {
+        let div = document.createElement("div");
+        div.classList.add("mb-5");
+
+        let title = document.createElement("h3");
+        title.textContent = poem.title;
+        div.appendChild(title);
+
+        let meta = document.createElement("p");
+        meta.textContent = `${poem.year} | ${poem.collection}`;
+        meta.classList.add("meta");
+        div.appendChild(meta);
+
+        for (let i = 0; i < poem.paragraphs.length; i++) {
+            let p = document.createElement("p");
+            p.innerHTML = poem.paragraphs[i];
+            div.appendChild(p);
+        }
+
+        this.poemsDiv.appendChild(div);
     }
 }
 
@@ -23,29 +56,44 @@ class Controller {
         this.view = view;
     }
 
-    getAllPoems(){
+    getAllPoems() {
         return this.model.poems;
     }
 
-    getPoemsByYear(year){
+    getPoemsByYear(year) {
         let filteredPoems = [];
-        for (let i = 0; i < this.model.poems.length; i++){
+        for (let i = 0; i < this.model.poems.length; i++) {
             if (this.model.poems[i].year == year) filteredPoems.push(this.model.poems[i]);
         }
         return filteredPoems;
     }
 
-    getPoemsByCollection(collection){
+    getPoemsByCollection(collection) {
         let filteredPoems = [];
-        for (let i = 0; i < this.model.poems.length; i++){
+        for (let i = 0; i < this.model.poems.length; i++) {
             if (this.model.poems[i].collection == collection) filteredPoems.push(this.model.poems[i]);
         }
         return filteredPoems;
     }
 
-    getPoemById(id){
+    getPoemById(id) {
         return this.model.poems[id];
+    }
+
+    displayPoems(poems) {
+        this.view.clearPoemsDiv();
+        for (let i = 0; i < poems.length; i++) {
+            this.view.addPoem(poems[i]);
+        }
+    }
+
+    async waitAndDisplayAllPoems() {
+        await this.model.fetchPoems();
+        this.displayPoems(this.model.poems);
     }
 }
 
-const app = new Controller(new Model(), new View())
+window.onload = (event) => {
+    const app = new Controller(new Model(), new View());
+    app.waitAndDisplayAllPoems()
+};
