6312e996422453fa5e66024e0f3828f3a7da497b
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-07-28 13:12:20 +0200

    loading all poems from json

--- poetry/script.js
+++ poetry/script.js
@@ -1,16 +1,22 @@
 class Model {
-    constructor() {}
-  }
-  
-  class View {
-    constructor() {}
-  }
-  
-  class Controller {
-    constructor(model, view) {
-      this.model = model
-      this.view = view
+    constructor() {
+        fetch("./data.json")
+            .then((res) => res.json())
+            .then((data) => {
+                this.poems = data;
+            });
     }
-  }
-  
-  const app = new Controller(new Model(), new View())
+}
+
+class View {
+    constructor() { }
+}
+
+class Controller {
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+    }
+}
+
+const app = new Controller(new Model(), new View())
