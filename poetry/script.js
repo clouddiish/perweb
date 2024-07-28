53e81a27238896e38e508c4ad4aba0659d89adca
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2024-07-28 15:04:13 +0200

    improvements to model, controler start

--- poetry/script.js
+++ poetry/script.js
@@ -1,21 +1,50 @@
 class Model {
     constructor() {
-        fetch("./data.json")
-            .then((res) => res.json())
-            .then((data) => {
-                this.poems = data;
-            });
+        this.poems = [];
+        this.fetchPoems();
+    }
+
+    async fetchPoems() {
+        const response = await fetch("./data.json")
+        let data = await response.json();
+        this.poems = data;
     }
 }
 
 class View {
-    constructor() { }
+    constructor() { 
+        this.poemsDiv = document.getElementById("poemsDiv");
+    }
 }
 
 class Controller {
     constructor(model, view) {
-        this.model = model
-        this.view = view
+        this.model = model;
+        this.view = view;
+    }
+
+    getAllPoems(){
+        return this.model.poems;
+    }
+
+    getPoemsByYear(year){
+        let filteredPoems = [];
+        for (let i = 0; i < this.model.poems.length; i++){
+            if (this.model.poems[i].year == year) filteredPoems.push(this.model.poems[i]);
+        }
+        return filteredPoems;
+    }
+
+    getPoemsByCollection(collection){
+        let filteredPoems = [];
+        for (let i = 0; i < this.model.poems.length; i++){
+            if (this.model.poems[i].collection == collection) filteredPoems.push(this.model.poems[i]);
+        }
+        return filteredPoems;
+    }
+
+    getPoemById(id){
+        return this.model.poems[id];
     }
 }
 
